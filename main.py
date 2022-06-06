faf33d94573ad1415d41949dd30ea5d0ff660c10
NUR <comics.marvel2015@yandex.ru>
2022-06-06 17:51:16 +0500

    first_02

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
-import telebot
+from aiogram import Bot,Dispatcher,executor,types
+import config
+import markups
+from db import Database
 
-bot = telebot.TeleBot("5534979217:AAH7rex-IQH6z53BAPNGhyvaMPIL6PyKEPY")
+bot = Bot(config.TOKEN)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    mes = f'Привет , <b>{message.from_user.first_name}</b>'
-    bot.send_message(message.chat.id ,mes, parse_mode='html')
+db = Database('database.db')
 
-@bot.message_handler()
-def get_user_text(message):
-    if(message.text == "Hello"):
-        bot.send_message(message.chat.id, "И тебе привет", parse_mode='html')
+dp = Dispatcher(bot)
 
-bot.polling(none_stop=True)
+@dp.message_handler(commands=['start'])
+async def start(message : types.Message):
+        if(not db.user_check(message.from_user.id)):
+                db.add_user(message.from_user.id)
+                await bot.send_message(message.from_user.id, "Укажите ваш ник")
+        else:
+                await bot.send_message(message.from_user.id , "Вы уже зарегистрированы",reply_markup=markups.mainMenu)
+
+@dp.message_handler()
+async def bot_message(message : types.Message):
+        if message.chat.type == 'private':
+                if(message.text == "ПРОФИЛЬ"):
+                        pass
+                else:
+                        if(db.get_signup(message.from_user.id) == "setnickname"):
+                                if(len(message.text) >15):
+                                        await bot.send_message(message.from_user.id, "Ник слишком большой")
+                                elif '@' in message.text or '/' in message.text:
+                                        await bot.send_message(message.from_user.id, "Вот енто @ и / нельзя")
+                                else:
+                                        db.set_nickname(message.from_user.id,message.text)
+                                        db.set_signup(message.from_user.id, "done")
+                                        await bot.send_message(message.from_user.id, "Ура вы зареганы!!",reply_markup=markups.mainMenu)
+                        else:
+                                await bot.send_message(message.from_user.id, "Больше нечего не умею")
+
+
+
+
+if (__name__=="__main__"):
+        executor.start_polling(dp,skip_updates=True)
